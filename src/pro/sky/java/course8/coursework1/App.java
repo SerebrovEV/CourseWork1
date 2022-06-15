5268657ea6c3a475aefe549faebf41d49937a230
Eugene <89818121788@mail.ru>
2022-06-15 22:43:57 +0300

    Third commit

--- src/pro/sky/java/course8/coursework1/App.java
+++ src/pro/sky/java/course8/coursework1/App.java
@@ -2,7 +2,17 @@ package pro.sky.java.course8.coursework1;
 
 public class App {
     public static void main(String[] args) {
-        Employee firstEmployee = new Employee("Serebrov", "Eugene", "Vladimirovich", "1", 100_000);
-Employee secondEmployee = new Employee("")
+        Employee embloyees[] = new Employee[10];
+        embloyees[0] = new Employee("Serebrov", "Eugene", "Vladimirovich", 1, 100_000);
+        embloyees[1] = new Employee("Smirnov", "Denis", "Borisovich", 2, 200_000);
+        embloyees[2] = new Employee("Baranov", "Maxim", "Olegovich", 4, 250_000);
+        embloyees[3] = new Employee("Simonov", "Artem", "Vladimirovich", 5, 100_000);
+        embloyees[4] = new Employee("Koshina", "Irina", "Alekseevna", 3, 300_000);
+        embloyees[5] = new Employee("Timofeeva", "Olga", "Victorovna", 1, 450_000);
+        embloyees[6] = new Employee("Pastushenko", "Denis", "Urevich", 2, 120_000);
+        embloyees[7] = new Employee("Vorobev", "Roman", "Vladimirovich", 1, 42_000);
+        embloyees[8] = new Employee("Karpunova", "Anna", "Andreevna", 5, 480_000);
+        embloyees[9] = new Employee("Serebrova", "Natalia", "Sergeevna", 2, 460_000);
+        System.out.println(embloyees);
     }
 }
--- src/pro/sky/java/course8/coursework1/Employee.java
+++ src/pro/sky/java/course8/coursework1/Employee.java
@@ -4,11 +4,11 @@ public class Employee {
     private String surname;
     private String name;
     private String secondName;
-    private String departmentName;
+    private int departmentName;
     private int salary;
     private static int id = 1;
 
-    public Employee(String surname, String name, String secondName, String departmentName, int salary) {
+    public Employee(String surname, String name, String secondName, int departmentName, int salary) {
         this.surname = surname;
         this.name = name;
         this.secondName = secondName;
@@ -29,9 +29,10 @@ public class Employee {
         return secondName;
     }
 
-    public String getDepartmentName() {
+    public int getDepartmentName() {
         return departmentName;
     }
+
     public int getSalary() {
         return salary;
     }
@@ -41,12 +42,23 @@ public class Employee {
     }
 
     public void setDepartmentName() {
-        this.departmentName = departmentName;
+        if (departmentName > 0 && departmentName < 6) {
+            this.departmentName = departmentName;
+        } else {
+            System.out.println("Неправильный номер отдела. Номера отделов могут быть только от 1 до 5");
+        }
     }
 
     public void setSalary() {
         this.salary = salary;
     }
 
-
+    @Override
+    public String toString() {
+        return "Сотрудник" + "Фамилия: " + surname + ", Имя: '" + name + '\'' +
+                ", Отчество='" + secondName + '\'' +
+                ", Номер отдела: " + departmentName +
+                ", Оклад: " + salary +
+                '}';
+    }
 }
