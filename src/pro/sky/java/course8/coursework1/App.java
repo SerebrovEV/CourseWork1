0413b381a47efe84eaaf0fef06d57d743dab31f9
Eugene <89818121788@mail.ru>
2022-06-19 21:32:02 +0300

    Harder CourseWork done

--- src/pro/sky/java/course8/coursework1/App.java
+++ src/pro/sky/java/course8/coursework1/App.java
@@ -1,34 +1,34 @@
 package pro.sky.java.course8.coursework1;
 
 public class App {
-    private static final Employee[] Employees = new Employee[10];
+    private static final Employee[] EMPLOYEES = new Employee[10];
 
     public static void main(String[] args) {
-        Employees[0] = new Employee("Serebrov", "Eugene", "Vladimirovich", 1, 100_000);
-        Employees[1] = new Employee("Smirnov", "Denis", "Borisovich", 2, 200_000);
-        Employees[2] = new Employee("Baranov", "Maxim", "Olegovich", 4, 250_000);
-        Employees[3] = new Employee("Simonov", "Artem", "Vladimirovich", 5, 100_000);
-        Employees[4] = new Employee("Koshina", "Irina", "Alekseevna", 2, 300_00);
-        Employees[5] = new Employee("Timofeeva", "Olga", "Victorovna", 1, 450_000);
-        Employees[6] = new Employee("Pastushenko", "Denis", "Urevich", 2, 120_005);
-        Employees[7] = new Employee("Vorobev", "Roman", "Vladimirovich", 1, 42_000);
-        Employees[8] = new Employee("Karpunova", "Anna", "Andreevna", 5, 480_000);
-        Employees[9] = new Employee("Serebrova", "Natalia", "Sergeevna", 2, 560_000);
+        EMPLOYEES[0] = new Employee("Serebrov", "Eugene", "Vladimirovich", 1, 100_000);
+        EMPLOYEES[1] = new Employee("Smirnov", "Denis", "Borisovich", 2, 200_000);
+        EMPLOYEES[2] = new Employee("Baranov", "Maxim", "Olegovich", 4, 250_000);
+        EMPLOYEES[3] = new Employee("Simonov", "Artem", "Vladimirovich", 5, 100_000);
+        EMPLOYEES[4] = new Employee("Koshina", "Irina", "Alekseevna", 2, 300_00);
+        EMPLOYEES[5] = new Employee("Timofeeva", "Olga", "Victorovna", 1, 450_000);
+        EMPLOYEES[6] = new Employee("Pastushenko", "Denis", "Urevich", 2, 120_005);
+        EMPLOYEES[7] = new Employee("Vorobev", "Roman", "Vladimirovich", 1, 42_000);
+        EMPLOYEES[8] = new Employee("Karpunova", "Anna", "Andreevna", 5, 480_000);
+        EMPLOYEES[9] = new Employee("Serebrova", "Natalia", "Sergeevna", 2, 560_000);
         basicTask();
         harderTask();
 
     }
 
     private static void printEmployee() {
-        for (int i = 0; i < Employees.length; i++) {
-            System.out.println(Employees[i]);
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            System.out.println(EMPLOYEES[i]);
         }
     }
 
     private static int allMonthSalary() {
         int allSalary = 0;
-        for (int i = 0; i < Employees.length; i++) {
-            allSalary += Employees[i].getSalary();
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            allSalary += EMPLOYEES[i].getSalary();
         }
         return allSalary;
     }
@@ -36,10 +36,10 @@ public class App {
     private static String minSalaryFio() {
         double minSalary = 1_000_000;
         String employeeFio = null;
-        for (int i = 0; i < Employees.length; i++) {
-            if (minSalary > Employees[i].getSalary()) {
-                minSalary = Employees[i].getSalary();
-                employeeFio = Employees[i].getFio();
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (minSalary > EMPLOYEES[i].getSalary()) {
+                minSalary = EMPLOYEES[i].getSalary();
+                employeeFio = EMPLOYEES[i].getFio();
             }
         }
         return employeeFio;
@@ -48,23 +48,22 @@ public class App {
     private static String maxSalaryFio() {
         double maxSalary = 0;
         String employeeFio = null;
-        for (int i = 0; i < Employees.length; i++) {
-            if (maxSalary < Employees[i].getSalary()) {
-                maxSalary = Employees[i].getSalary();
-                employeeFio = Employees[i].getFio();
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (maxSalary < EMPLOYEES[i].getSalary()) {
+                maxSalary = EMPLOYEES[i].getSalary();
+                employeeFio = EMPLOYEES[i].getFio();
             }
         }
         return employeeFio;
     }
 
     private static double midSalary() {
-        double midSalary = (double) allMonthSalary() / Employees.length;
-        return midSalary;
+    return allMonthSalary() / EMPLOYEES.length;
     }
 
     private static void getFioEmployees() {
-        for (int i = 0; i < Employees.length; i++) {
-            System.out.println("Ф.И.О. " + (i + 1) + ": " + Employees[i].getFio());
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            System.out.println("Ф.И.О. " + (i + 1) + ": " + EMPLOYEES[i].getFio());
 
         }
     }
@@ -80,19 +79,19 @@ public class App {
     }
 
     private static void increaseSalary(double percent) {
-        for (int i = 0; i < Employees.length; i++) {
-            double increase = Employees[i].getSalary() + Employees[i].getSalary() * (percent / 100);
-            Employees[i].setSalary(increase);
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            double increase = EMPLOYEES[i].getSalary() + EMPLOYEES[i].getSalary() * (percent / 100);
+            EMPLOYEES[i].setSalary(increase);
         }
     }
 
     private static String searchDepartmentMinSalaryFio(int numberDepartment) {
         double minSalary = 1_000_000;
         String minSalaryFio = null;
-        for (int i = 0; i < Employees.length; i++) {
-            if (numberDepartment == Employees[i].getDepartmentName() && minSalary > Employees[i].getSalary()) {
-                minSalaryFio = Employees[i].getFio();
-                minSalary = Employees[i].getSalary();
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (numberDepartment == EMPLOYEES[i].getDepartmentName() && minSalary > EMPLOYEES[i].getSalary()) {
+                minSalaryFio = EMPLOYEES[i].getFio();
+                minSalary = EMPLOYEES[i].getSalary();
             }
         }
         return minSalaryFio;
@@ -101,10 +100,10 @@ public class App {
     private static String searchDepartmentMaxSalaryFio(int numberDepartment) {
         double maxSalary = 0;
         String maxSalaryFio = null;
-        for (int i = 0; i < Employees.length; i++) {
-            if (numberDepartment == Employees[i].getDepartmentName() && maxSalary < Employees[i].getSalary()) {
-                maxSalary = Employees[i].getSalary();
-                maxSalaryFio = Employees[i].getFio();
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (numberDepartment == EMPLOYEES[i].getDepartmentName() && maxSalary < EMPLOYEES[i].getSalary()) {
+                maxSalary = EMPLOYEES[i].getSalary();
+                maxSalaryFio = EMPLOYEES[i].getFio();
             }
         }
         return maxSalaryFio;
@@ -112,9 +111,9 @@ public class App {
 
     private static double allMonthSalaryDepartment(int numberDepartment) {
         double allSallary = 0;
-        for (int i = 0; i < Employees.length; i++) {
-            if (numberDepartment == Employees[i].getDepartmentName()) {
-                allSallary += Employees[i].getSalary();
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (numberDepartment == EMPLOYEES[i].getDepartmentName()) {
+                allSallary += EMPLOYEES[i].getSalary();
             }
         }
         return allSallary;
@@ -122,47 +121,46 @@ public class App {
 
     private static double midSalaryDepartment(int numberDepartment) {
         double allSallary = 0;
-        double midSallary;
         int j = 0;
-        for (int i = 0; i < Employees.length; i++) {
-            if (numberDepartment == Employees[i].getDepartmentName()) {
-                allSallary += Employees[i].getSalary();
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (numberDepartment == EMPLOYEES[i].getDepartmentName()) {
+                allSallary += EMPLOYEES[i].getSalary();
                 j++;
             }
         }
-        midSallary = allSallary / j;
-        return midSallary;
+        return allSallary / j;
+
     }
 
     private static void increaseSalaryDepartment(int numberDepartment, double percent) {
-        for (int i = 0; i < Employees.length; i++) {
-            if (numberDepartment == Employees[i].getDepartmentName()) {
-                double increase = Employees[i].getSalary() + Employees[i].getSalary() * (percent / 100);
-                Employees[i].setSalary(increase);
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (numberDepartment == EMPLOYEES[i].getDepartmentName()) {
+                double increase = EMPLOYEES[i].getSalary() + EMPLOYEES[i].getSalary() * (percent / 100);
+                EMPLOYEES[i].setSalary(increase);
             }
         }
     }
 
     private static void printEmployeeDepartment(int numberDepartment) {
-        for (int i = 0; i < Employees.length; i++) {
-            if (numberDepartment == Employees[i].getDepartmentName()) {
-                System.out.println(Employees[i].DepartmentInfo());
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (numberDepartment == EMPLOYEES[i].getDepartmentName()) {
+                System.out.println(EMPLOYEES[i].DepartmentInfo());
             }
         }
     }
 
     private static void searchEmployeeWithMinParameter(double parameter) {
-        for (int i = 0; i < Employees.length; i++) {
-            if (parameter > Employees[i].getSalary()) {
-                System.out.println(Employees[i].DepartmentInfo());
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (parameter > EMPLOYEES[i].getSalary()) {
+                System.out.println(EMPLOYEES[i].DepartmentInfo());
             }
         }
     }
 
     private static void searchEmployeeWithMaxParameter(double parameter) {
-        for (int i = 0; i < Employees.length; i++) {
-            if (parameter < Employees[i].getSalary()) {
-                System.out.println(Employees[i].DepartmentInfo());
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (parameter < EMPLOYEES[i].getSalary()) {
+                System.out.println(EMPLOYEES[i].DepartmentInfo());
             }
         }
     }
